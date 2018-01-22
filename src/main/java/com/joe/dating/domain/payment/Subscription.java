ebfe1fa16b436b05bdcc3600a37282f02141573e
s5802496 <joeydeluca@hotmail.com>
2018-01-22 07:41:25 -0500

    bug fix

--- src/main/java/com/joe/dating/domain/payment/Subscription.java
+++ src/main/java/com/joe/dating/domain/payment/Subscription.java
@@ -26,7 +26,7 @@ public class Subscription extends DatingEntity {
     @Column(name = "processor_subscription_id")
     private String processorSubscriptionId;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "id", cascade = CascadeType.ALL)
     private List<Transaction> transactions;
 
     public User getUser() {
