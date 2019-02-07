1d311a3ca5b97e8f6fee1419533eee6ac5817df3
s5802496 <joeydeluca@hotmail.com>
2019-02-06 22:29:58 -0500

    circle ci

--- src/test/java/com/joe/dating/domain/user/UserServiceIT.java
+++ src/test/java/com/joe/dating/domain/user/UserServiceIT.java
@@ -13,12 +13,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = {DatingApplication.class})
