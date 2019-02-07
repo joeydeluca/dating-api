54587c6fcfa92c0f732c28117d9b013c81e47f2a
s5802496 <joeydeluca@hotmail.com>
2019-02-06 22:31:37 -0500

    circle ci

--- src/test/java/com/joe/dating/DatingApplicationTests.java
+++ src/test/java/com/joe/dating/DatingApplicationTests.java
@@ -3,12 +3,10 @@ package com.joe.dating;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DatingApplicationTests {
